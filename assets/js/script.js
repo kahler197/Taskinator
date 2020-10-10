1285ae02eab43b4fafb04e381af9ee1e00ecae6b
Alex Kahler <kahler197@gmail.com>
2020-10-09 21:14:47 -0500

    correct js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,17 +1,14 @@
-var buttonE1 = document.querySelector("#save-task");
-var tasksToDoE1 = document.querySelector("#tasks-to-do");
+var formEl = document.querySelector("#task-form");
+var tasksToDoEl = document.querySelector("#tasks-to-do");
 
-var createTaskHandler = function() {
+var createTaskHandler = function(event) {
+
+    event.preventDefault();
+
     var listItemEl = document.createElement("li");
     listItemEl.className = "task-item";
     listItemEl.textContent = "This is a new task.";
     tasksToDoEl.appendChild(listItemEl);
-  }
+  };
 
-
-  buttonE1.addEventListener("click", function() {
-    var listItemE1 = document.createElement("li");
-    listItemE1.className = "task-item";
-    listItemE1.textContent = "this is a new task.";
-    tasksToDoE1.appendChild(listItemE1);
-});
+  formEl.addEventListener("submit", createTaskHandler);
