ee77de07d2fcc99881168a5879212bb3be2184ee
Alex Kahler <kahler197@gmail.com>
2020-10-10 22:02:47 -0500

    drag style and function

--- assets/js/script.js
+++ assets/js/script.js
@@ -41,6 +41,7 @@ var taskFormHandler = function(event) {
     listItemEl.className = "task-item";
 
     listItemEl.setAttribute("data-task-id", taskIdCounter);
+    listItemEl.setAttribute("draggable", true);
 
     var taskInfoEl = document.createElement("div");
     taskInfoEl.className = "task-info";
@@ -170,12 +171,53 @@ var taskStatusChangeHandler = function(event) {
   }
 };
 
+var dragTaskHandler = function(event) {
+  var taskId = event.target.getAttribute("data-task-id");
+  event.dataTransfer.setData("text/plain", taskId);
+
+  var getId = event.dataTransfer.getData("text/plain");
+  console.log("getId:", getId, typeof getId);
+}
+
+var dropZoneDragHandler = function(event) {
+  var taskListEl = event.target.closest(".task-list");
+  if (taskListEl) {
+    event.preventDefault();
+    }
+
+    taskListEl.setAttribute("style", "background: rgba(68, 233, 255, 0.7); border-style: dashed;");
+};
+
+var dropTaskHandler = function(event) {
+  var id = event.dataTransfer.getData("text/plain");
  
+  var draggableElement = document.querySelector("[data-task-id='" + id + "']");
+  var dropZoneEl = event.target.closest(".task-list");
+  var statusType = dropZoneEl.id;
+  var statusSelectEl = draggableElement.querySelector("select[name='status-change']");
+  
+  if (statusType === "tasks-to-do") {
+    statusSelectEl.selectedIndex = 0;
+  } 
+  else if (statusType === "tasks-in-progress") {
+    statusSelectEl.selectedIndex = 1;
+  } 
+  else if (statusType === "tasks-completed") {
+    statusSelectEl.selectedIndex = 2;
+  }
 
-    
+   dropZoneEl.appendChild(draggableElement);
 
+   dropZoneEl.removeAttribute("style");
+};
 
-
+var dragLeaveHandler = function(event) {
+  var taskListEl = event.target.closest(".task-list");
+    if (taskListEl) {
+    taskListEl.removeAttribute("style");
+}
+  
+};
 
   formEl.addEventListener("submit", taskFormHandler);
 
@@ -183,4 +225,10 @@ var taskStatusChangeHandler = function(event) {
 
   pageContentEl.addEventListener("change", taskStatusChangeHandler);
 
-  
+  pageContentEl.addEventListener("dragstart", dragTaskHandler);
+
+  pageContentEl.addEventListener("dragover", dropZoneDragHandler);
+
+  pageContentEl.addEventListener("drop", dropTaskHandler);
+
+  pageContentEl.addEventListener("dragleave", dragLeaveHandler);
